5c2d69dfde61fd550e4b4956b7e1c6d0b529646c
spokwn <115673262+spokwn@users.noreply.github.com>
2024-08-06 03:27:39 -0300

    Update bamsignatures.ps1

--- bamsignatures.ps1
+++ bamsignatures.ps1
@@ -11,8 +11,7 @@ Write-Host @"
  ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝    ╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝                                                                          
 "@ -ForegroundColor Magenta
 Write-Host ""
-Write-Host "                                                      Made by spokwn kjj - " -ForegroundColor Gray -NoNewline
-Write-Host -ForegroundColor DarkMagenta "discord.gg/astralmc"
+Write-Host "                                                      Made by spokwn"
 Write-Host ""
 
 function Get-OldestConnectTime {
@@ -26,7 +25,6 @@ function Get-OldestConnectTime {
         return $null
     }
 }
-
 
 function Get-DeviceMappings {
     $DynAssembly = New-Object System.Reflection.AssemblyName('SysUtils')
@@ -89,7 +87,6 @@ function Get-FileSignature {
     }
 }
 
-
 $oldestConnectTime = Get-OldestConnectTime
 
 $deviceMappings = Get-DeviceMappings
@@ -108,7 +105,7 @@ foreach($ii in $bv){
 }
 
 if ($Users.Count -eq 0) {
-    Write-Host "No se encontraron entradas BAM. Es posible que este sistema no sea compatible."
+    Write-Host "No BAM entries found. This system may not be compatible."
     exit
 }
 
@@ -160,4 +157,288 @@ Foreach ($Sid in $Users) {
 
 $ErrorActionPreference = 'Continue'
 
-$Bam | Out-GridView -PassThru -Title "BAM key entries $($Bam.count) - User TimeZone: ($UserTime)"
+$ContenidoHtml = @'
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>BAM Signature</title>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
+      rel="stylesheet"
+    />
+    <style>
+      :root {
+        --background-color: #121212;
+        --surface-color: #1e1e1e;
+        --primary-color: #64b5f6;
+        --text-color: #e0e0e0;
+        --hover-color: #2196f3;
+      }
+      body {
+        font-family: "Roboto", sans-serif;
+        background-color: var(--background-color);
+        color: var(--text-color);
+        margin: 0;
+        padding: 20px;
+        transition: all 0.1s ease;
+      }
+      h1 {
+        color: var(--primary-color);
+        text-align: center;
+        margin-bottom: 30px;
+        font-weight: 300;
+        font-size: 2.5em;
+      }
+      .search-container {
+        margin-bottom: 20px;
+        background-color: var(--surface-color);
+        padding: 15px;
+        border-radius: 8px;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+      }
+      #search {
+        width: calc(100% - 30px);
+        padding: 10px 15px;
+        border: none;
+        border-radius: 4px;
+        background-color: var(--background-color);
+        color: var(--text-color);
+        font-family: "Roboto", sans-serif;
+        transition: all 0.1s ease;
+      }
+      #search:hover {
+        outline: none;
+        box-shadow: 0 0 0 2px #64b4f6d8;
+      }
+      #search:focus {
+        outline: none;
+        box-shadow: 0 0 0 2px var(--primary-color);
+      }
+      table {
+        width: 100%;
+        border-collapse: separate;
+        border-spacing: 0 8px;
+        margin-top: 20px;
+      }
+      th,
+      td {
+        padding: 15px;
+        text-align: left;
+        transition: all 0.24s ease;
+      }
+      th {
+        background-color: var(--surface-color);
+        color: var(--primary-color);
+        font-weight: 700;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        cursor: pointer;
+        position: relative;
+      }
+      th:hover {
+        background-color: var(--hover-color);
+        color: var(--background-color);
+      }
+      th.asc::after,
+      th.desc::after {
+        position: absolute;
+        right: 8px;
+        top: 50%;
+        transform: translateY(-50%);
+        font-size: 0.8em;
+      }
+      th.asc::after {
+        content: "▲";
+      }
+      th.desc::after {
+        content: "▼";
+      }
+      tr {
+        background-color: var(--surface-color);
+        transition: all 0.24s ease;
+      }
+      tr:hover {
+        transform: translateY(-2px);
+        scale: 1.013;
+        box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);
+      }
+      @keyframes fadeIn {
+        from {
+          opacity: 0;
+          transform: translateY(20px);
+        }
+        to {
+          opacity: 1;
+          transform: translateY(0);
+        }
+      }
+      .fade-in {
+        /* animation: fadeIn 0.15s ease-out; */
+      }
+      .credit-info {
+        text-align: center;
+        margin-top: 20px;
+        font-size: 0.9em;
+        color: var(--text-color);
+        opacity: 0.7;
+      }
+      .credit-info a {
+        color: var(--primary-color);
+        text-decoration: none;
+        position: relative;
+        transition: color 0.18s ease-in-out;
+      }
+
+      .credit-info a:hover {
+        color: #33a3ff;
+      }
+      .credit-info a::after {
+        content: "";
+        position: absolute;
+        width: 100%;
+        height: 2px;
+        background-color: #33a3ff;
+        color: #33a3ff;
+        bottom: -1px;
+        left: 0;
+        transform: scaleX(0);
+        transform-origin: right;
+        transition: transform 0.18s ease-in-out;
+      }
+
+      .credit-info a:hover::after {
+        transform: scaleX(1);
+        transform-origin: left;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="search-container fade-in">
+      <input type="text" id="search" placeholder="Search..." />
+    </div>
+
+    <table id="entriesTable" class="fade-in">
+      <thead>
+        <tr>
+          <th data-sort="time">Last Execution</th>
+          <th data-sort="path">Path</th>
+          <th data-sort="signature">Digital Signature</th>
+          <th data-sort="fileName">File Name</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+
+    <div class="credit-info fade-in">
+      Made by espouken
+      <br>
+      <a href="https://discordapp.com/users/1149799913727721485" target="_blank">Discord</a>
+      <br>
+      <a href="https://github.com/spokwn" target="_blank">Github</a>
+    </div>
+
+    <script>
+      const entries = [
+'@
+
+foreach ($entry in $Bam) {
+    $escapedTime = $entry.'Last Execution User Time'.Replace("\", "\\")
+    $escapedPath = $entry.Path.Replace("\", "\\")
+    $escapedSignature = $entry.'Digital Signature'.Replace("\", "\\")
+    $escapedFileName = $entry.'File Name'.Replace("\", "\\")
+    $ContenidoHtml += @"
+        {
+          time: "$escapedTime",
+          path: "$escapedPath",
+          signature: "$escapedSignature",
+          fileName: "$escapedFileName"
+        },
+"@
+}
+
+$ContenidoHtml += @'
+      ];
+
+      let currentSort = { column: "time", direction: "asc" };
+
+      function populateTable(data) {
+        const tbody = document.querySelector("#entriesTable tbody");
+        tbody.innerHTML = "";
+        data.forEach((entry, index) => {
+          const row = document.createElement("tr");
+          row.className = "fade-in";
+          row.style.animationDelay = `${index * 0.1}s`;
+          row.innerHTML = `
+                    <td>${entry.time}</td>
+                    <td>${entry.path}</td>
+                    <td>${entry.signature}</td>
+                    <td>${entry.fileName}</td>
+                `;
+          tbody.appendChild(row);
+        });
+      }
+
+      function applyFilters() {
+        let filteredEntries = [...entries];
+        const searchTerm = document
+          .getElementById("search")
+          .value.toLowerCase();
+
+        filteredEntries = filteredEntries.filter((entry) =>
+          Object.values(entry).some((value) =>
+            value.toLowerCase().includes(searchTerm)
+          )
+        );
+
+        filteredEntries.sort((a, b) => {
+          const aValue = a[currentSort.column];
+          const bValue = b[currentSort.column];
+          if (currentSort.direction === "asc") {
+            return aValue.localeCompare(bValue);
+          } else {
+            return bValue.localeCompare(aValue);
+          }
+        });
+
+        populateTable(filteredEntries);
+        updateSortIndicators();
+      }
+
+      function updateSortIndicators() {
+        document.querySelectorAll("th").forEach((th) => {
+          th.classList.remove("asc", "desc");
+          if (th.dataset.sort === currentSort.column) {
+            th.classList.add(currentSort.direction);
+          }
+        });
+      }
+
+      document.getElementById("search").addEventListener("input", applyFilters);
+
+      document.querySelectorAll("th[data-sort]").forEach((th) => {
+        th.addEventListener("click", () => {
+          const column = th.dataset.sort;
+          if (currentSort.column === column) {
+            currentSort.direction =
+              currentSort.direction === "asc" ? "desc" : "asc";
+          } else {
+            currentSort.column = column;
+            currentSort.direction = "asc";
+          }
+          applyFilters();
+        });
+      });
+
+      applyFilters();
+    </script>
+  </body>
+</html>
+'@
+
+
+$htmlFilePath = Join-Path $env:TEMP "BAMKeyEntries.html"
+$ContenidoHtml | Out-File -FilePath $htmlFilePath -Encoding UTF8
+
+Start-Process $htmlFilePath
